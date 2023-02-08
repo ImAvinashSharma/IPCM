0e50e4b22e83d57af053f4c082c1d04b5cc89c65
imAviSharma <avinash.s@comakeit.com>
2023-02-08 15:39:02 +0530

    Added something

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
+import VaultItems from "./VaultItems";
 
 export default function Dashboard() {
   const [error, setError] = useState();
@@ -197,7 +198,7 @@ export default function Dashboard() {
               <li className="rounded-sm">
                 <Link
                   className="flex items-center p-2 space-x-3 rounded-md"
-                  onClick={(e) => signOut}
+                  onClick={signOut}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -221,85 +222,7 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="container mx-auto mt-12">
-        <div>Vault Items</div>
-
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="p-4">
-                  <div className="flex items-center">
-                    <input
-                      id="checkbox-all-search"
-                      type="checkbox"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label htmlFor="checkbox-all-search" className="sr-only">
-                      checkbox
-                    </label>
-                  </div>
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  application name
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  username
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  created at
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  last used
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Available
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td className="w-4 p-4">
-                  <div className="flex items-center">
-                    <input
-                      id="checkbox-table-search-1"
-                      type="checkbox"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                      htmlFor="checkbox-table-search-1"
-                      className="sr-only"
-                    >
-                      checkbox
-                    </label>
-                  </div>
-                </td>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Instagram
-                </th>
-                <td className="px-6 py-4">kings</td>
-                <td className="px-6 py-4">today</td>
-                <td className="px-6 py-4">today</td>
-                <td className="flex items-center px-6 py-4 space-x-3">
-                  <Link
-                    to="/edit"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    Edit
-                  </Link>
-                  <Link
-                    to="/delete"
-                    className="font-medium text-red-600 dark:text-red-500 hover:underline"
-                  >
-                    Remove
-                  </Link>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <VaultItems />
       </div>
     </div>
   );
