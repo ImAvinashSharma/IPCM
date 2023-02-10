a97552f80659c205cfb660de5305f4908f90f183
avi9360 <savinash406@gmail.com>
2023-02-10 22:38:04 +0530

    Bug fix #4

--- client/src/components/Features.js
+++ client/src/components/Features.js
@@ -6,8 +6,8 @@ function Features() {
       <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">Features</h2>
       <div className="flex items-center flex-wrap mb-20">
         <div className="w-full md:w-1/2">
-          <h4 className="text-3xl text-gray-800 font-bold mb-3">Exercise Metric</h4>
-          <p className="text-gray-600 mb-8">Our Smart Health Monitoring Wristwatch is able to capture you vitals while you exercise. You can create different category of exercises and can track your vitals on the go.</p>
+          <h4 className="text-3xl text-gray-800 font-bold mb-3">Password management</h4>
+          <p className="text-gray-600 mb-8">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
         </div>
         <div className="w-full md:w-1/2">
           <img height="320" width="600" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMBkqLmGoB7b_A8uUyTI25VBHQ-AkZ92KoFA&usqp=CAU" alt="Monitoring" />
@@ -19,15 +19,15 @@ function Features() {
           <img height="320" width="600" src="https://cheekymunkey.co.uk/wp-content/uploads/2022/04/Password-blog.jpg" alt="Reporting" />
         </div>
         <div className="w-full md:w-1/2 pl-10">
-          <h4 className="text-3xl text-gray-800 font-bold mb-3">Reporting</h4>
-          <p className="text-gray-600 mb-8">Our Smart Health Monitoring Wristwatch can generate a comprehensive report on your vitals depending on your settings either daily, weekly, monthly, quarterly or yearly.</p>
+          <h4 className="text-3xl text-gray-800 font-bold mb-3">Password manager</h4>
+          <p className="text-gray-600 mb-8">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
         </div>
       </div>
 
       <div className="flex items-center flex-wrap mb-20">
         <div className="w-full md:w-1/2">
-          <h4 className="text-3xl text-gray-800 font-bold mb-3">Syncing</h4>
-          <p className="text-gray-600 mb-8">Our Smart Health Monitoring Wristwatch allows you to sync data across all your mobile devices whether iOS, Android or Windows OS and also to your laptop whether MacOS, GNU/Linux or Windows OS.</p>
+          <h4 className="text-3xl text-gray-800 font-bold mb-3">Password</h4>
+          <p className="text-gray-600 mb-8">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
         </div>
         <div className="w-full md:w-1/2">
           <img height="320" width="600" src="https://2wtech.com/wp-content/uploads/2020/11/pwmanage.png" alt="Syncing" />
